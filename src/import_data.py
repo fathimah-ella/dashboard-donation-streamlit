e52004dccd5605ad5fb59ae7dc79376b6c901ce3
fathimah-ella <faatheellaa@gmail.com>
2025-06-15 23:25:59 +0700

    Update import_data.py

--- src/import_data.py
+++ src/import_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def load_data():
     # Baca file Excel
-    transaksi_qris = pd.read_excel("D:\\KP\\sobatberbagi.com_dashboard\\data\\transaksi_qris.xlsx", skiprows=1)
-    transaksi_manual = pd.read_excel("D:\\KP\\sobatberbagi.com_dashboard\\data\\transaksi_manual.xlsx", skiprows=1)
+    transaksi_qris = pd.read_excel("data/transaksi_qris.xlsx", skiprows=1)
+    transaksi_manual = pd.read_excel("data/transaksi_manual.xlsx", skiprows=1)
 
     return transaksi_qris, transaksi_manual
